23eab756c4016f1569ae825f2db794494a2affbf
Matin Baki <m9652973@gmail.com>
2025-02-21 03:34:01 +0330

    Add sidebar

--- script/app.js
+++ script/app.js
@@ -2,6 +2,8 @@ const button = document.querySelector('.button_class');
 const footer = document.querySelector('.mainfooter');
 const emailBox = document.querySelector('.emailbox');
 const the_form = document.querySelector('.sendemail_box_container');
+
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (!entry.isIntersecting) {
@@ -27,3 +29,44 @@ observer.observe(button);
 button.addEventListener('click', function() {
     this.classList.add('active'); 
 });
+
+
+
+
+
+const openMenu = document.querySelector('.openMenu');
+const closeMenuOn = document.querySelector('.closeOn');
+const sidebar = document.getElementById('sildeBarMenu');
+
+
+openMenu.addEventListener('click', () => {
+        console.log("1")
+        closeMenuOn.style.display='block';
+        sidebar.style.display='flex';
+        openMenu.style.width='0';
+        
+
+});
+
+closeMenuOn.addEventListener('click', () => {
+    openMenu.style.width='2rem';
+    sidebar.style.display='none';
+    closeMenuOn.style.display='none';
+    
+});
+
+
+
+function checkViewportWidth() {
+    if (window.innerWidth > 576 &&     sidebar.style.displa!='none'    ) {
+        sidebar.style.display='none';
+        closeMenuOn.style.display='none';
+        openMenu.style.width='2rem';
+
+        } 
+
+}
+
+window.addEventListener('resize', checkViewportWidth);
+
+checkViewportWidth();
